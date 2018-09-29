7f6f87b95139d0e6df71e0704eab08d783668aec
rickyes <mail@zhoumq.cn>
2018-09-29 21:07:35 +0800

    chore: update ci

--- benchmarks/parse.js
+++ benchmarks/parse.js
@@ -4,13 +4,13 @@ const os = require('os');
 const childProcess = require('child_process');
 const v = require('..');
 
-console.log('==========================');
-console.log('v8-json: ' + require('../package.json').version);
-console.log('CPU: ' + os.cpus().length);
-console.log('OS: ' + os.platform() + ' ' + os.arch());
-console.log('node version: ' + process.version);
-console.log('current commit: ' + childProcess.execSync('git rev-parse --short HEAD'));
-console.log('==========================');
+// console.log('==========================');
+// console.log('v8-json: ' + require('../package.json').version);
+// console.log('CPU: ' + os.cpus().length);
+// console.log('OS: ' + os.platform() + ' ' + os.arch());
+// console.log('node version: ' + process.version);
+// console.log('current commit: ' + childProcess.execSync('git rev-parse --short HEAD'));
+// console.log('==========================');
 
 let str = '{"test-1": "-1",';
 for(let i = 0; i<1000; i++){
